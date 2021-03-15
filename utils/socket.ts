8c11ee7240c347065809268d32a6d86d6585902c
BlacKlyExactly <supreme24d@gmail.com>
2021-03-15 13:05:50 +0100

    Socket.io production fix

--- utils/socket.ts
+++ utils/socket.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 const port: string = process.env.NODE_ENV === 'production' ? "" : ":3000";
 
-const endpoint: string = `${process.env.NEXT_PUBLIC_API_DOMAIN}${port}`;
+const endpoint: string = `${process.env.NEXT_PUBLIC_DOMAIN}${port}`;
 const socket: SocketIOClient.Socket = io(endpoint);
 
 export default socket;
