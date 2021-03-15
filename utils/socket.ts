8a68422b2e87a3b1b4264dab3e5e979dfae61f62
BlacKlyExactly <supreme24d@gmail.com>
2021-03-15 12:41:33 +0100

    Socket.io production fix

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const endpoint: string = process.env.NEXT_PUBLIC_DOMAIN;
+const endpoint: string = process.env.NEXT_PUBLIC_DOMAIN
 const socket: SocketIOClient.Socket = io(endpoint);
 
 export default socket;
